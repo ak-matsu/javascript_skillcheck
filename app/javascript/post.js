9663f1074ad69781d4b19c4b5d97b0e646e50f5d
ak-matsu <sandbox.matsu@gmail.com>
2021-03-11 23:20:33 +0900

    コメントアウト

--- app/javascript/post.js
+++ app/javascript/post.js
@@ -1,7 +1,13 @@
 function post (){
+  // クリックしたことを認識するために.getElementById("submit_btn")を用いて取得
   const submit = document.getElementById("submit_btn");
+  // クリックによるイベントはclickを用いる
   submit.addEventListener("click", (e) => {
+    // コンソール上（ログ）に表示させる。
+    // index.html.erbでformにはnew_articleというのidが付与されているため、getElementByIdを用いて取得します。
     console.log(document.getElementById("new_article"));
+    // ブラウザ上に用意されている送信ボタンが反応してしまい
+    // 処理が重複してしまうのでe.preventDefault();でデフォルト送信をキャンセルする。
     e.preventDefault();
   });
 }
