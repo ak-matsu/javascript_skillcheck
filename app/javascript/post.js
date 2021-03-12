415ef3928b20bb11ed374ed731816e46339d6479
ak-matsu <sandbox.matsu@gmail.com>
2021-03-12 22:13:04 +0900

    問題2-3：入力した文字の数をコンソールに出力してください。

--- app/javascript/post.js
+++ app/javascript/post.js
@@ -5,8 +5,8 @@ function post (){
   // キーボード操作があった場合にイベント発火。
   // そのときに用いるイベントハンドラーがkeyup
   article_text.addEventListener("keyup", () => {
-    // 取得した要素に含まれる値をvalueを用いてconsole.logへ出力させる
-    console.log(article_text.value);
+    // 取得した要素に含まれる値をvalueを用いてconsole.logへ出力させる 
+    console.log(article_text.value.length);
     });
   }
 
