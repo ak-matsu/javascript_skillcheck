11c9c9aff10ebbb46361705f6f19b581b067fd4a
ak-matsu <sandbox.matsu@gmail.com>
2021-03-14 00:51:39 +0900

    1-3. サーバーサイドにリクエストを送ろう

--- app/javascript/post.js
+++ app/javascript/post.js
@@ -5,7 +5,20 @@ function post (){
   submit.addEventListener("click", (e) => {
     // コンソール上（ログ）に表示させる。
     // index.html.erbでformにはnew_articleというのidが付与されているため、getElementByIdを用いて取得します。
-    console.log(document.getElementById("new_article"));
+    const formData = new FormData(document.getElementById("new_article"));
+
+    // Ajaxに利用するオブジェクトを生成
+    const XHR = new XMLHttpRequest();
+
+    // .openメソッドでAjaxに関する情報を初期化、URLを設定
+    XHR.open("POST","/articles",true);
+
+    // レスポンスとしてjsonを指定
+    XHR.responseType = "json"
+
+    // Ajaxで送信
+    XHR.send(formData);
+
 
     // ブラウザ上に用意されている送信ボタンが反応してしまい
     // 処理が重複してしまうのでe.preventDefault();でデフォルト送信をキャンセルする。
