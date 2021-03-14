c5ad17a62735f0b2527cc797f168743e3f43b241
ak-matsu <sandbox.matsu@gmail.com>
2021-03-14 21:47:16 +0900

    2-4. カウントした文字数を表示しよう

--- app/javascript/count.js
+++ app/javascript/count.js
@@ -2,19 +2,19 @@ function count (){
   // 入力したことを認識するために.getElementById("article_text")を用いて取得
   const articleText = document.getElementById("article_text");
 
-  // 入力した文字数を表示するためにchar_numを取得
-  const charNum = document.getElementById("char_num")
 
-  
-  console.log(char_num.innerHTML)
   // 取得した要素に何らかのキーボード操作があった場合にイベント発火。
   // そのときに用いるイベントハンドラーが"keyup"である。
   articleText.addEventListener("keyup",() => {
 
-    // 実際に打ち込んだ値を（articleText）valueで用いて取得し
-    // コンソールログで表示させる。
-    // lengthを用いるとその文字数をカウントした結果が、戻り値として得られる。
-    console.log(articleText.value.innerHTML);
+  // 入力されている文字数をカウントした結果を取得。
+  const countVal = articleText.value.length;
+
+  // 入力した文字数を表示するためにchar_numを取得
+  const charNum = document.getElementById("char_num")  
+
+  // 取得した要素に対して、${countVal}文字を、innerHTMLを用いて置き換えている。
+  charNum.innerHTML = `${countVal}文字`;
   });
 }
 
