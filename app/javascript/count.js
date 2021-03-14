eac9f8313f57c8dbe353cc9f721e7747b4c5eaea
ak-matsu <sandbox.matsu@gmail.com>
2021-03-14 21:14:42 +0900

    2-2. イベントを発火させよう

--- app/javascript/count.js
+++ app/javascript/count.js
@@ -1,6 +1,15 @@
 function count (){
+  // 入力したことを認識するために.getElementById("article_text")を用いて取得
+  const articleText = document.getElementById("article_text");
 
-  console.log("文字数カウント機能を実装")
+  // 取得した要素に何らかのキーボード操作があった場合にイベント発火。
+  // そのときに用いるイベントハンドラーが"keyup"である。
+  articleText.addEventListener("keyup",() => {
+
+    // 実際に打ち込んだ値を（articleText）valueで用いて取得し
+    // コンソールログで表示させる。
+    console.log(articleText.value);
+  });
 }
 
 window.addEventListener("load",count);
